d9392af4dd70de2e3692fe4c588ae164fff8f353
Connaigh <Connaigh@gmail.com>
2018-05-23 12:50:33 +1000

    Initial commit

---
+++ app/src/main/java/com/example/whitakcj/g2/splash.java
@@ -0,0 +1,17 @@
+package com.example.whitakcj.g2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
